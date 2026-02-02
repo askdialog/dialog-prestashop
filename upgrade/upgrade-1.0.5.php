6c81f7bdee356e57ebda21ee778f2a7f5f55ff8a
Axel Paillaud <contact@axelweb.fr>
2026-02-02 14:36:46 +0100

    add migration to register missing hook

---
+++ upgrade/upgrade-1.0.5.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * 2026 Dialog
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Axel Paillaud <contact@axelweb.fr>
+ * @copyright 2026 Dialog
+ * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+/**
+ * Upgrade script for version 1.0.5
+ * Registers PostHog tracking hooks that were accidentally lost during a merge
+ *
+ * @param AskDialog $module
+ *
+ * @return bool
+ */
+function upgrade_module_1_0_5($module)
+{
+    return $module->registerHook('actionCartUpdateQuantityBefore')
+        && $module->registerHook('actionValidateOrder');
+}
